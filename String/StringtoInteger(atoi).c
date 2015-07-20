39e98173f4053b4560623d2641bdf5e0614407d6
yangjin-unique <jinyang.hust@gmail.com>
2015-07-20 20:09:33 +0800

    correct atoi

--- String/StringtoInteger(atoi).c
+++ String/StringtoInteger(atoi).c
@@ -8,21 +8,31 @@ Notes: It is intended for this problem to be specified vaguely (ie, no given inp
 Update (2015-02-10):
 The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
 */
+#define MAX		2147483647
+#define MIN		-2147483648
 
 int myAtoi(char* str) {
 	int neg = 0;
 	char *p = str;
-	int res = 0;
+	long res = 0;
 
-	while (*p == ' ') {p++};
+	while (*p == ' ') {p++;}
 	if (*p == '-') {
 		neg = 1;
 		p++;
 	}
-	
-	while (*p != '\0' && *p > '0' && *p < '9') {
-		res = res*10 + (*p - '0'); 
+	else if (*p == '+') {
+	    p++;
 	}
+	
+	while (*p != '\0' && *p >= '0' && *p <= '9') {
+		res = res*10 + (*p - '0');
+		if (!neg && res > MAX) return MAX;
+		if (neg && -res <  MIN) return MIN;
+		p++;
+	}
+	
 	if (neg) return -res;
+	
 	return res;
 }
