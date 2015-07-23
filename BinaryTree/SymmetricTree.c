33a3c0a2b8a40718ec2bd401f06dee1d606eda47
yangjin-unique <jinyang.hust@gmail.com>
2015-07-23 19:36:39 +0800

    add iterative solution for isSymmetricTree

--- BinaryTree/SymmetricTree.c
+++ BinaryTree/SymmetricTree.c
@@ -28,6 +28,9 @@ Bonus points if you could solve it both recursively and iteratively.
  * };
  */
 
+/**
+ * method 1: recursive 
+ */
 bool isSymm(struct TreeNode *node1, struct TreeNode *node2) {
 	/* first compare node1 with node2 */
 	if (node1 == NULL || node2 == NULL) {
@@ -55,3 +58,54 @@ bool isSymmetric(struct TreeNode* root) {
 
     return isSymm(root->left, root->right);
 }
+
+
+/**
+ * method 2: iterative, inorder traverse for root->left and root->right
+ */
+ bool isSymmetric(struct TreeNode* root) {
+ 	struct TreeNode **stack1;
+ 	struct TreeNode **stack2;
+ 	struct TreeNode *node1, *node2;
+ 	int top1 =0, top2 = 0;
+ 	bool ret = 1; 
+
+ 	if (root == NULL) return 1;
+ 	node1 = root->left;
+ 	node2 = root->right;
+ 	if (node1 == node2) return 1;
+ 	stack1 = (struct TreeNode **) malloc(sizeof(struct TreeNode *) * 512);
+ 	stack2 = (struct TreeNode **) malloc(sizeof(struct TreeNode *) * 512);
+    if (node1 != NULL)
+ 	    stack1[top1++] = node1;
+ 	if (node2 != NULL)
+ 	    stack2[top2++] = node2;
+ 	while (top1 > 0 && top2 > 0) {
+ 		node1 = stack1[--top1];
+ 		node2 = stack2[--top2];
+ 		
+ 		while (node1 != NULL && node2 != NULL) {
+ 			if (node1->val != node2->val) {
+ 				ret = 0;
+ 				break;
+ 			}
+ 			if (node1->right != NULL)
+ 				stack1[top1++] = node1->right;
+ 			if (node2->left != NULL)
+ 				stack2[top2++] = node2->left;
+ 			node1 = node1->left;
+ 			node2 = node2->right;
+ 		}
+ 		if (node1 != node2) {
+ 			ret = 0;
+ 			break;
+ 		} /* node1 = node2 = NULL */
+ 	}
+ 	if (ret == 1) {
+ 		if (top1 != top2)
+ 			ret = 0;
+ 	}
+ 	free(stack1);
+ 	free(stack2);
+ 	return ret;
+ }
