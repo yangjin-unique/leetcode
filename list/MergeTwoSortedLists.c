24ca9bb4496dae8794d2b4e155bc4f68ca6543c7
yangjin-unique <jinyang.hust@gmail.com>
2015-08-26 18:03:20 +0800

    simplify merge list

--- list/MergeTwoSortedLists.c
+++ list/MergeTwoSortedLists.c
@@ -6,32 +6,21 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode *p1, *p2, *cur, *prev;
-    struct ListNode *nh = NULL;
-    
-    if (l1 == NULL) return l2;
-    if (l2 == NULL) return l1;
-    p1 = l1; p2= l2;
-    while (p1 != NULL && p2 != NULL) {
-        if (p1->val > p2->val) {
-            cur = p2;
-            p2 = p2->next;
+    struct ListNode dummy = {0, NULL}; /* use a dummy node to simplify process */
+    struct ListNode *cur = &dummy;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val > l2->val) {
+            cur->next = l2;
+            l2 = l2->next;
         }
         else {
-            cur = p1;
-            p1 = p1->next;
+            cur->next = l1;
+            l1 = l1->next;
         }
-        if (nh == NULL) {
-            nh = cur;
-            nh->next = NULL;
-            prev = nh;
-        }
-        else {
-            prev->next = cur;
-            prev = cur;
-        }
+	cur = cur->next;
     }
-    if (p1 == NULL) prev->next = p2;
-    if (p2 == NULL) prev->next = p1;
-    return nh;
+    if (l1 != NULL) cur->next = l1;
+    if (l2 != NULL) cur->next = l2;
+    return dummy.next;
 }
