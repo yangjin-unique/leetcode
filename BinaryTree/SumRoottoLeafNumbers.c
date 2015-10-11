52f0f30752c2b942951bf7ea364703c8a59c946e
yangjin-unique <jinyang.hust@gmail.com>
2015-10-11 17:57:53 +0800

    add python, c++, simpler c version to SumRoototLeafNumbers

--- BinaryTree/SumRoottoLeafNumbers.c
+++ BinaryTree/SumRoottoLeafNumbers.c
@@ -25,6 +25,7 @@ Return the sum = 12 + 13 = 25
  * };
  */
 /**
+ * method 1:
  * recursion method
  * @sum represents one root-to-leaf path, like 12, 13.
  * @totalSum total sum.
@@ -49,3 +50,97 @@ int sumNumbers(struct TreeNode* root) {
     calcTree(root, sum, &totalSum);
     return totalSum;
 }
+
+/**
+ * method 2: simplier version
+ */
+int calcTree(struct TreeNode *root, int sum) {
+
+	if (root == NULL)
+		return 0;
+	if (root->left == NULL && root->right == NULL) {
+		return sum + root->val;
+	}
+	sum += root->val;
+	return calcTree(root->left, sum*10) + calcTree(root->right, sum*10);
+}
+
+int sumNumbers(struct TreeNode* root) {
+	int sum = 0;
+
+    return calcTree(root, sum);
+}
+
+/**
+ * c++ solution
+ */
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        int totalSum = 0;
+        
+        calcTree(root, sum, &totalSum);
+        return totalSum;
+    }
+    
+    void calcTree(TreeNode *root, int sum, int *totalSum) {
+        if (root == NULL) return;
+        if (root->left == NULL && root->right == NULL) {
+            *totalSum += sum*10 + root->val;
+            return;
+        }
+        sum = sum*10 + root->val;
+        calcTree(root->left, sum, totalSum);
+        calcTree(root->right, sum, totalSum);
+        return;
+    }
+};
+
+
+/**
+ * python code
+ */
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        totalSum = 0;
+        
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        sum = 0;
+        self.totalSum = 0;
+        if not root:
+            return 0;
+        self.calcTree(root, sum);
+        return self.totalSum;
+        
+    def calcTree(self, root, sum):
+        if root.left == None and root.right == None:
+            self.totalSum += sum*10 + root.val;
+            return;
+        sum = sum*10 + root.val;
+        if root.left:
+            self.calcTree(root.left, sum);
+        if root.right:
+            self.calcTree(root.right, sum);    
+
+
